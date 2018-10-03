e7800f8826b146527f1c4765e7634a9691556e0a
Burton428 <ADAMBURTON428@users.noreply.github.com>
2018-10-03 11:06:16 -0500

    update

---
+++ Intermediate26/Intermediate26/Source.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	cout << fixed << setprecision(2);
+
+	//declare array
+	double prices[10] = { 10.5, 25.5, 9.75, 6.0, 35.0, 100.4, 10.65, .56, 14.75, 4.78 };
+	//declare variable
+	double increase = 0.0;
+	
+	//update prices
+	cout << "Enter increase percentage (for example, enter 15 for 15%): ";
+	cin >> increase;
+	for (double i = 0; i < increase; i++) {
+
+		
+	
+	}//end for
+
+	//display contents of array
+	
+	//end for
+
+	system("pause");
+	return 0;
+}
